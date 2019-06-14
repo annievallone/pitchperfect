d5dd67ce8fec6c01103ac81abab2d0bebe4ba97d
Annie Vallone <annievallone@gmail.com>
2019-06-14 14:56:30 -0400

    added play sounds view controller class

---
+++ pitchperfect/PlaySoundsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PlaySoundsViewController.swift
+//  pitchperfect
+//
+//  Created by annie vallone on 6/14/19.
+//  Copyright © 2019 annie wilson. All rights reserved.
+//
+
+import UIKit
+
+class PlaySoundsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
